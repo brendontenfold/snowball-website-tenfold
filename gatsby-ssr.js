036dda50c0c9b62b72103fb759dda0da6a4ed8a0
Daniel Gibson <danielgibson@Mac-mini.local>
2019-12-20 15:40:17 +1300

    geo fix attempt #2

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -3,14 +3,14 @@ export function onRenderBody(
   { setPostBodyComponents }
 ) {
   setPostBodyComponents([
-  <script
-  dangerouslySetInnerHTML={{
-        __html=`
-      (function(g,e,o,t,l,y){
-      var l=g.getElementsByTagName(e)[0],y=g.createElement(e); y.async=true;
-      y.src='//geo-targetly.com/geocontent?id=-LwVI_y1Pd76sqACFNmC&refurl='+g.referrer+'&winurl='+encodeURIComponent(window.location);
-      l.parentNode.insertBefore(y,l);
-      })(document,'script','style','head');
-  `}}/>,
+    dangerouslySetInnerHTML={ __html=`  
+    <script>
+        (function(g,e,o,t,l,y){
+        var l=g.getElementsByTagName(e)[0],y=g.createElement(e); y.async=true;
+        y.src='//geo-targetly.com/geocontent?id=-LwVI_y1Pd76sqACFNmC&refurl='+g.referrer+'&winurl='+encodeURIComponent(window.location);
+        l.parentNode.insertBefore(y,l);
+        })(document,'script','style','head');
+    </script>
+    `},
   ]);
 }
