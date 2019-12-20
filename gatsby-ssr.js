fa7d1cfba2f37ea655da6e5f6f890b482c3a8edf
Daniel Gibson <danielgibson@Mac-mini.local>
2019-12-20 15:41:50 +1300

    Geo fix attempt #3

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,16 +1,16 @@
-import React from "react";
-export function onRenderBody(
-  { setPostBodyComponents }
-) {
-  setPostBodyComponents([
-    dangerouslySetInnerHTML={ __html=`  
-    <script>
-        (function(g,e,o,t,l,y){
-        var l=g.getElementsByTagName(e)[0],y=g.createElement(e); y.async=true;
-        y.src='//geo-targetly.com/geocontent?id=-LwVI_y1Pd76sqACFNmC&refurl='+g.referrer+'&winurl='+encodeURIComponent(window.location);
-        l.parentNode.insertBefore(y,l);
-        })(document,'script','style','head');
-    </script>
-    `},
-  ]);
-}
+// import React from "react";
+// export function onRenderBody(
+//   { setPostBodyComponents }
+// ) {
+//   setPostBodyComponents([
+//     dangerouslySetInnerHTML={ __html=`  
+//     <script>
+//         (function(g,e,o,t,l,y){
+//         var l=g.getElementsByTagName(e)[0],y=g.createElement(e); y.async=true;
+//         y.src='//geo-targetly.com/geocontent?id=-LwVI_y1Pd76sqACFNmC&refurl='+g.referrer+'&winurl='+encodeURIComponent(window.location);
+//         l.parentNode.insertBefore(y,l);
+//         })(document,'script','style','head');
+//     </script>
+//     `},
+//   ]);
+// }
